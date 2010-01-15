ad9ef8de439476e62ad00db302ef2eac027c6cdb
Vic Cherubini <vmc@leftnode.com>
2010-01-15 17:35:49 -0600

    Added the basic methods for load and save.

--- DataModel.php
+++ DataModel.php
@@ -1,5 +1,36 @@
 <?php
 
 abstract class DataModel {
-
+	private $adapter = NULL;
+	
+	public function __construct(DataAdapter $adapter) {
+		
+	}
+	
+	public function __destruct() {
+		
+	}
+	
+	public function setDataAdapter(DataAdapter $adapter) {
+		$this->adapter = $adapter;
+		return $this;
+	}
+	
+	public function load(DataObject $object, $pkey) {
+		$this->hasDataAdapter();
+		
+		
+	}
+	
+	public function save(DataObject $object) {
+		$this->hasDataAdapter();
+		
+	}
+	
+	private function hasDataAdapter() {
+		if ( NULL === $this->adapter ) {
+			throw new Exception('No DataAdapter has been set. Please set one first.');
+		}
+		return true;
+	}
 }
