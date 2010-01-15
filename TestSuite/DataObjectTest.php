1cd302527b3810248eee0219c4a971b9928850eb
Vic Cherubini <vmc@leftnode.com>
2010-01-15 16:51:29 -0600

    Started with the basic test suite.

---
+++ TestSuite/DataObjectTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once '../DataObject.php';
+
+class DataObjectTest extends PHPUnit_Framework_TestCase {
+
+
+	public function testCreateDataObject() {
+
+		return true;
+	}
+}
