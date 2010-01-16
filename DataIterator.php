f779bf55e04364ee92b6b3a890e5d5550f26618e
Vic Cherubini <vmc@leftnode.com>
2010-01-16 10:59:43 -0600

    Got the iterator to work and wrote a lot of tests for it.

--- DataIterator.php
+++ DataIterator.php
@@ -1,5 +1,189 @@
 <?php
 
-class DataIterator {
+class DataIterator implements Iterator {
+	private $data_list = array();
+	private $key = 0;
+	private $length = 0;
+	private $filter = array();
+	private $filter_count = 0;
+	private $limit = -1;
+	private $page = 0;
+	
+	private $data_object = NULL;
+	
+	public function __construct(array $data_list, DataObject $data_object) {
+		$this->data_list = $data_list;
+		$this->key = 0;
+		$this->length = count($data_list);
+		$this->data_object = $data_object;
+	}
+	
+	public function __clone() {
+		$this->length = count($this->data_list);
+		$this->rewind();
+	}
+	
+	public function getList() {
+		return $this->data_list;
+	}
+	
+	public function getObject() {
+		return $this->data_object;
+	}
+	
+	public function rewind() {
+		if ( $this->page > 0 && $this->limit > -1 ) {
+			$this->key = ( ($this->page-1) * $this->limit );
+			$this->length = ($this->page * $this->limit );
+		} else {
+			$this->key = 0;
+		}
 
+		reset($this->data_list);
+	}
+	
+	public function current() {
+		if ( true === isset($this->data_list[$this->key]) ) {
+			return $this->data_list[$this->key];
+		}
+		
+		return current($this->data_list);
+	}
+	
+	public function last() {
+		return end($this->data_list);
+	}
+	
+	public function key() {
+		return $this->key;
+	}
+	
+	public function next() {
+		$this->key++;
+		return next($this->data_list);
+	}
+	
+	public function valid() {
+		return ( $this->key != $this->length && true === isset($this->data_list[$this->key]) );
+	}
+
+	public function page($page) {
+		$this->page = intval($page);
+		return $this;
+	}
+
+	public function limit($limit) {
+		$this->limit = $limit;
+		return $this;
+	}
+
+	public function length() {
+		return $this->length;
+	}
+	
+	public function fetch() {
+		$result_list = $this->data_list;
+		
+		if ( true === $this->hasFilter() ) {
+			$result_list = array();
+			foreach ( $this->data_list as $list_item ) {
+				if ( true === $this->applyFilter($list_item->get()) ) {
+					$result_list[] = $list_item;
+				}
+			}
+
+			$this->filter = array();
+			$this->filter_count = 0;
+		}
+
+		return (new DataIterator($result_list, $this->getObject()));
+	}
+	
+	public function filter($field, $value) {
+		$this->limit = -1;
+		$this->filter[] = array($field, $value);
+		$this->filter_count++;
+		return $this;
+	}
+	
+	private function hasFilter() {
+		if ( true === is_array($this->filter) && $this->filter_count > 0 ) {
+			return true;
+		}
+		return false;
+	}
+	
+	private function applyFilter($data_array) {
+		if ( false === $this->hasFilter() ) {
+			return true;
+		}
+		
+		$passed = false;
+		$match_count = 0;
+
+		foreach ( $this->filter as $filter ) {
+			$field = $filter[0];
+			$value = $filter[1];
+
+			/* Get the operator cheaply. */
+			$op_bits = explode(' ', $field);
+			
+			$field = trim($op_bits[0]);
+			$op = trim($op_bits[1]);
+			
+			if ( true == isset($data_array[$field]) ) {
+				switch ( $op ) {
+					case '==':
+					case '=': {
+						if ( $data_array[$field] == $value ) {
+							$match_count++;
+						}						
+						break;
+					}
+					
+					case '!=':
+					case '<>': {
+						if ( $data_array[$field] != $value ) {
+							$match_count++;
+						}
+						break;
+					}
+					
+					case '>=': {
+						if ( $data_array[$field] >= $value ) {
+							$match_count++;
+						}
+						break;
+					}
+					
+					case '<=': {
+						if ( $data_array[$field] <= $value ) {
+							$match_count++;
+						}
+						break;
+					}
+					
+					case '<': {
+						if ( $data_array[$field] < $value ) {
+							$match_count++;
+						}
+						break;
+					}
+					
+					case '>': {
+						if ( $data_array[$field] > $value ) {
+							$match_count++;
+						}
+						break;
+					}
+				}
+				
+				if ( $match_count == $this->filter_count ) {
+					$passed = true;
+				}
+			}
+		}
+		
+		return $passed;
+	}
 }
--- TestSuite/DataIteratorTest.php
+++ TestSuite/DataIteratorTest.php
@@ -1,10 +1,95 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
+require_once 'DataObject.php';
 require_once 'DataIterator.php';
 
 class DataIteratorTest extends PHPUnit_Framework_TestCase {
-	public function testSomething() {
-		return true;
+
+	protected $data_list = array();
+	
+	protected function setUp() {
+		$this->data_list = array(
+			$this->getMockDataObject('Vic Cherubini', 'vmc@leftnode.com', 25),
+			$this->getMockDataObject('Bob Saget', 'bob@saget.com', 53),
+			$this->getMockDataObject('Randy Newton', 'randy@newton.com', 18),
+			$this->getMockDataObject('Jon Stewart', 'jon@stewart.com', 48),
+			$this->getMockDataObject('Bob Goldweight', 'bob@goldweight.com', 48),
+			$this->getMockDataObject('Jeremy Jones', 'jones@jeremy.com', 18),
+			$this->getMockDataObject('Rod Stiffington', 'rod@stiffington.com', 48),
+			$this->getMockDataObject('Jack Mayoffer', 'jack@mayoffer.com', 32),
+			$this->getMockDataObject('Mike Hunt', 'mike@hunt.com', 65),
+			$this->getMockDataObject('Poor Fellow', 'poor@fellow.com', 48),
+		);
+	}
+	
+	public function testIteratorMustBeArray() {
+		try {
+			$di = new DataIterator('abc', $this->getMockDataObject());
+		} catch ( Exception $e ) {
+			$this->assertNull($di);
+			return true;
+		}
+		
+		$this->fail();
+	}
+	
+	public function testIteratorIsInitiallyAnEmptyArray() {
+		$di = new DataIterator(array(), $this->getMockDataObject());
+		$this->assertEquals($di->getList(), array());
+	}
+	
+	public function testIteratorLength() {
+		$di = new DataIterator($this->data_list, $this->getMockDataObject());
+		$length = count($this->data_list);
+		$this->assertEquals($di->length(), $length);
+	}
+	
+	public function testFetchWithoutFilters() {
+		$di1 = new DataIterator($this->data_list, $this->getMockDataObject());
+		$di2 = $di1->fetch();
+		$this->assertEquals($di1, $di2);
+	}
+	
+	public function testFetchWithSingleFilter() {
+		$mock = $this->getMockDataObject();
+		$first = reset($this->data_list);
+		$di1 = new DataIterator($this->data_list, $mock);
+		$di2 = new DataIterator(array($first), $mock);
+		
+		$di3 = $di1->filter('name = ?', $first->getName())->fetch();
+		
+		$this->assertEquals($di2, $di3);
+		$this->assertEquals($di2->length(), $di3->length());
+	}
+	
+	public function testFetchWithMultipleFilters() {
+		$mock = $this->getMockDataObject();
+		
+		/* Not sure if this is the best way to do things. */
+		$manually_found_result_list = array();
+		foreach ( $this->data_list as $list_item ) {
+			if ( 10 == $list_item->getFavoriteNumber() && 48 == $list_item->getAge() ) {
+				$manually_found_result_list[] = $list_item;
+			}
+		}
+		
+		$di1 = new DataIterator($this->data_list, $mock);
+		$di2 = new DataIterator($manually_found_result_list, $mock);
+		
+		$di3 = $di1->filter('favorite_number = ?', 10)->filter('age = ?', 48)->fetch();
+		
+		$this->assertEquals($di2, $di3);
+		$this->assertEquals($di2->length(), $di3->length());
+	}
+	
+	protected function getMockDataObject($name=NULL, $email=NULL, $age=18) {
+		$data_object = $this->getMockForAbstractClass('DataObject');
+		$data_object->setName($name);
+		$data_object->setEmail($email);
+		$data_object->setAge($age);
+		$data_object->setFavoriteNumber(10);
+		
+		return $data_object;
 	}
 }
