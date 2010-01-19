ba52157c3af1c0458935c177905191a7ee315543
Vic Cherubini <vmc@leftnode.com>
2010-01-19 16:32:36 -0600

    New test suite for the data model.

---
+++ TestSuite/DataModelTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once 'DataModel.php';
+
+
+class DataModelTest extends PHPUnit_Framework_TestCase {
+	protected function setUp() {
+	}
+
+	public function testBasicGetterSetter() {
+	}
+}
