b62683d8d12a4edde76368d1ad93011c037102fe
Vic Cherubini <vmc@leftnode.com>
2010-01-20 06:27:13 -0600

    Wrote some more examples of using the datarelationship class.

--- main.php
+++ main.php
@@ -4,6 +4,7 @@ require_once 'DataModelerException.php';
 
 require_once 'DataObject.php';
 require_once 'DataModel.php';
+require_once 'DataRelationship.php';
 require_once 'DataIterator.php';
 require_once 'DataAdapterPdoMysql.php';
 
@@ -11,31 +12,34 @@ class ProductObject extends DataObject {
 }
 
 class ProductModel extends DataModel {
+	protected function init() {
+		$this->setTable('product');
+		$this->setPkey('product_id');
+	}
 }
 
 try {
 	
 
-	$data_adapter = new DataAdapterPdoMysql(DataAdapterPdo::DRIVER_MYSQL, 'localhost', 'data_modeler', 'root', 'dba89da');
+	$data_adapter = new DataAdapterPdoMysql('localhost', 'data_modeler', 'root', 'dba89da');
 	$data_adapter->connect();
-	//$data_adapter->query('INSERT INTO `product` (`date_create`, `date_modify`, name, price) VALUES(?, ?, ?, ?)', array(time(), 0, "Jon's O'Reilly Honda", 1595));
-	//echo $data_adapter->insertId();
-	
-	//$result = $data_adapter->query('SELECT * FROM `product` WHERE product_id = ?', array(1));
-	//while ( $row = $result->fetch() ) {
-	//	print_r($row);
-	//}
 	
 	$product = new ProductObject();
 	$product_model = new ProductModel($data_adapter, 'product');
 
-	$product_model->load($product, 3);
-	$product->setName('changed name');
-	$product_model->save($product);
-	
-	//$product->setName('new product')->setPrice(1895);
-	//$product_model->save($product);
 
+	$data_relationship = new DataRelationship($product_model);
+	
+	$iterator = $data_relationship->where('product_id = ?', 3)->where('name <> ?', 'some new name')->find($product);
+
+	foreach ( $iterator as $obj ) {
+		print_r($obj);
+	}
+
+	//$product_model->load($product, 3);
+	//$product->setName('new name');
+	//$product_model->save($product);
+	
 } catch ( DataModelerException $e ) {
 	exit($e . PHP_EOL);
 }
