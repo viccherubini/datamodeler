dbc1a1add3fa0153584e5e92f81ce3724766976c
Vic Cherubini <vmc@leftnode.com>
2010-01-20 06:30:50 -0600

    Beginnings of the tests for the datamodel, looks like the phpunit database testing capabilities are a bit limited now.

--- TestSuite/DataModelTest.php
+++ TestSuite/DataModelTest.php
@@ -5,9 +5,7 @@ require_once 'DataModel.php';
 
 
 class DataModelTest extends PHPUnit_Framework_TestCase {
-	protected function setUp() {
-	}
-
-	public function testBasicGetterSetter() {
+	public function testModel() {
+		return true;
 	}
 }
