bee0c769026ef38a11e6ca53315c1424809341d8
Vic Cherubini <vmc@leftnode.com>
2010-01-21 07:16:41 -0600

    Started testing/demoing the data_queryier.

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require_once 'DataModelerException.php';
 
 require_once 'DataObject.php';
 require_once 'DataModel.php';
-require_once 'DataRelationship.php';
+require_once 'DataQueryier.php';
 require_once 'DataIterator.php';
 require_once 'DataAdapterPdoMysql.php';
 
@@ -25,21 +25,17 @@ try {
 	$data_adapter->connect();
 	
 	$product = new ProductObject();
-	$product_model = new ProductModel($data_adapter, 'product');
+	$product_model = new ProductModel($data_adapter);
 
 
-	$data_relationship = new DataRelationship($product_model);
+	$data_queryier = new DataQueryier($product_model);
 	
-	$iterator = $data_relationship->where('product_id <> ?', 3)->find($product);
+	$iterator = $data_queryier->where('product_id <> ?', 3)->find($product);
 
-	foreach ( $iterator as $obj ) {
-		print_r($obj);
-	}
+	//foreach ( $iterator as $obj ) {
+	//	print_r($obj);
+	//}
 
-	//$product_model->load($product, 3);
-	//$product->setName('new name');
-	//$product_model->save($product);
-	
 } catch ( DataModelerException $e ) {
 	exit($e . PHP_EOL);
 }
