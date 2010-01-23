b130996d63e2e087794ddaa478e8e47924ab4be9
Vic Cherubini <vmc@leftnode.com>
2010-01-23 08:02:49 -0600

    Better intro page.

--- main.php
+++ main.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'configure.php';
+
 require_once 'DataModelerException.php';
 
 require_once 'DataObject.php';
@@ -14,12 +16,17 @@ class Product extends DataObject {
 
 try {
 
+	$db = new DataAdapterPdoMysql($db_hostname, $db_database, $db_username, $db_password);
+	$db->connect();
+
 	$product = new Product();
-	$product->setName('here is the product name');
+	$model = new DataModel($db);
 	
+	//$product = $model->load($product, 1);
+	$product->setName('brand new product')->setPrice(1856)->setDateCreate(560500);
+	$id = $model->save($product);
 	
-	
-	
+	echo $id . PHP_EOL;
 } catch ( DataModelerException $e ) {
 	exit($e . PHP_EOL);
 }
