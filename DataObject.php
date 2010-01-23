04f1d7c2f8a6dfd006a1d367cf5eb775626b0dc3
Vic Cherubini <vmc@leftnode.com>
2010-01-23 06:35:03 -0600

    Massive updates to this file, added all of the methods to access special properties of the class: id() to get/set the id, pkey(), table(), model(), methodCache(), hasDate(), and children(). These all provide access to get the value if no argument is passed, and to set the value to the first argument passed. Works really nicely, I think.

--- DataObject.php
+++ DataObject.php
@@ -1,10 +1,16 @@
 <?php
 
 abstract class DataObject {
-	protected $id = 0;
-	protected $model = array();
-	protected $method_cache = array();
-	protected $has_date = true;
+	private $id = 0;
+	private $pkey = NULL;
+	private $table = NULL;
+	private $model = array();
+	private $method_cache = array();
+	private $children = array();
+	private $has_date = true;
+	
+	const TABLE_ROOT = '';
+	
 	
 	public function __construct() {
 		$this->init();
@@ -30,10 +36,18 @@ abstract class DataObject {
 				$this->method_cache[$k] = $v;
 				return $v;
 			} else {
-				/* Else assume its a set with the first element of $argv. */
 				$v = current($argv);
-				$this->__set($k, $v);
-				$this->method_cache[$k] = $v;
+				
+				/* If the key is the pkey of the object, don't allow that to be set. */
+				$pkey = $this->pkey();
+				if ( $k == $pkey ) {
+					$this->id($v);
+				} else {
+					/* Else assume its a set with the first element of $argv. */
+					$this->__set($k, $v);
+					$this->method_cache[$k] = $v;
+				}
+				
 				return $this;
 			}
 		}
@@ -53,42 +67,123 @@ abstract class DataObject {
 	
 	
 	/**
-	 * GETTERS
+	 * ANONYMOUS GETTERS AND SETTERS
 	 */
 	
-	public function get() {
-		return $this->model;
+
+	public function model() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->model;
+		} else {
+			$model = func_get_arg(0);
+			if ( false === is_array($model) ) {
+				$model = array();
+			}
+			
+			$pkey = $this->pkey();
+			if ( true === isset($model[$pkey]) ) {
+				$id = $model[$pkey];
+				$this->id($id);
+				unset($model[$pkey]);
+			}
+			
+			$this->model = $model;
+			return $this;
+		}
+	}
+	
+	public function id() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->id;
+		} else {
+			$id = func_get_arg(0);
+			$this->id = $id;
+			return $this;
+		}
+	}
+	
+	public function table() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->table;
+		} else {
+			$table = func_get_arg(0);
+			$this->table = self::TABLE_ROOT . $table;
+			return $this;
+		}
+	}
+	
+	public function pkey() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->pkey;
+		} else {
+			$pkey = func_get_arg(0);
+			$this->pkey = $pkey;
+			return $this;
+		}
+	}
+	
+	public function methodCache() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->method_cache;
+		} else {
+			$method_cache = func_get_arg(0);
+			if ( false === is_array($method_cache) ) {
+				$method_cache = array();
+			}
+			$this->method_cache = $method_cache;
+			return $this;
+		}
+	}
+	
+	public function children() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->children;
+		} else {
+			$children = func_get_arg(0);
+			if ( false === is_array($children) ) {
+				$children = array();
+			}
+			$this->children = $children;
+			return $this;
+		}
 	}
 
-	public function getId() {
-		return $this->id;
+	public function hasDate() {
+		$argc = func_num_args();
+		if ( 0 === $argc ) {
+			return $this->has_date;
+		} else {
+			$has_date = func_get_arg(0);
+			if ( false !== $has_date && true !== $has_date ) {
+				$has_date = true;
+			}
+			$this->has_date = true;
+			return $this;
+		}
 	}
 	
-	public function getHasDate() {
-		return $this->has_date;
-	}
 	
-	/**
-	 * SETTERS
-	 */
-	
-	public function setId($id) {
-		$this->id = $id;
-		return $this;
-	}
-	
-	public function set(array $model) {
-		$this->model = $model;
-		return $this;
-	}
 	
 	/**
 	 * CLASS MODIFIERS
 	 */
 	
 	public function init() {
-		$this->id = 0;
-		$this->model = array();
-		$this->method_cache = array();
+		$class_name = strtolower(get_class($this));
+		$this->id(0)
+			->model(array())
+			->methodCache(array())
+			->children(array())
+			->hasDate(true)
+			->table($class_name)
+			->pkey($class_name . '_id');
+		
+		return true;
 	}
 }
