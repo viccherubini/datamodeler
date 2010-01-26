e094520dd8d8ebd9b08d5049a984df7a377cbd40
Vic Cherubini <vmc@leftnode.com>
2010-01-26 14:59:10 -0600

    Removed the empty functions for now.

--- DataAdapterSqlizer.php
+++ DataAdapterSqlizer.php
@@ -6,15 +6,4 @@ class DataAdapterSqlizer {
 	public function __construct() {
 		
 	}
-	
-	
-	public function createInsertQuery(DataObject $object) {
-		
-		
-	}
-	
-	public function createUpdateQuery(DataObject $object) {
-		
-	}
-	
 }
