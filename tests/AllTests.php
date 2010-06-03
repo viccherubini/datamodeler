190a45cac2b5262f127651b1eff01ce81dfd26b4
Vic Cherubini <vmc@leftnode.com>
2010-06-02 21:26:49 -0500

    Included the right ModelTest.php file, made this part of the DataModelerTest namespace and added the ModelTest testsuite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,12 +1,16 @@
 <?php
 
 declare(encoding='UTF-8');
+namespace DataModelerTest;
 
+require_once 'ModelTest.php';
 
 class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Tests');
 
+		$suite->addTestSuite('\DataModelerTest\ModelTest');
+		
 		return $suite;
 	}
 }
