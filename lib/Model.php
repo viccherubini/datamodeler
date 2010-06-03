620bcb875d3d70913155e99dc892fab9c9b858f3
Vic Cherubini <vmc@leftnode.com>
2010-06-03 04:57:09 -0500

    Added method id() to get/set the primary key value (id) of the model (not all models will have them) and added an empty pkey method.

--- lib/Model.php
+++ lib/Model.php
@@ -35,6 +35,15 @@ abstract class Model {
 		return $this->datetype;
 	}
 
+
+	public function id($id = NULL) {
+		if ( false === empty($id) ) {
+			$this->id = $id;
+		}
+		return $this->id;
+	}
+
+
 	public function model(array $model = array()) {
 		if ( false !== current($model) || ( count($model) > 0 ) ) {
 			$this->model = $model;
@@ -42,10 +51,16 @@ abstract class Model {
 		return $this->model;
 	}
 	
+	
+	public function pkey($pkey = NULL) {
+		
+	}
+
 
 	public function table($table = NULL) {
 		$table = trim($table);
 		if ( false === empty($table) ) {
+			$table = str_replace('`', NULL, $table);
 			$this->table = $table;
 		}
 		return $this->table;
