fc88633c94b5d333b7f5c6dcc8a511771ebcf216
Vic Cherubini <vmc@leftnode.com>
2010-06-02 21:25:20 -0500

    Some custom tests.

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace DataModelerTest;
+
+class TestCase extends \PHPUnit_Framework_TestCase {
+	public static function assertArray($a, $message = '') {
+		self::assertThat(is_array($a), self::isTrue(), $message);
+	}
+	
+	
+	public static function assertEmptyArray($a, $message = '') {
+		self::assertArray($a);
+		self::assertEquals(0, count($a), $message);
+	}
+	
+	
+	public static function assertNotEmptyArray($a, $message = '') {
+		self::assertArray($a);
+		self::assertGreaterThan(0, count($a), $message);
+	}
+	
+	protected function buildMockModel() {
+		$model = $this->getMockForAbstractClass('\DataModeler\Model');
+		
+		return $model;
+	}
+}
