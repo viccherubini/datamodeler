bf8b4c6b8ec099d168a00c80413ffd0ded98ae1c
Vic Cherubini <vmc@leftnode.com>
2010-06-03 04:57:37 -0500

    Much updated test suite for the model.

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -7,15 +7,48 @@ require_once 'lib/Model.php';
 
 class ModelTest extends TestCase {
 
-	public function testTableNameCanBeSet() {
-		$table_name = 'products';
 	
-		$model = $this->buildMockModel();
-		$model->table($table_name);
-		
-		$this->assertEquals($table_name, $model->table()); 
-	}
 
+
+	public function testDatetypeIsTimestampByDefault() {
+		$model = $this->buildMockModel();
+		$model->datetype(100);
+		
+		$this->assertEquals(\DataModeler\Model::DATETYPE_TIMESTAMP, $model->datetype());
+	}
+	
+	
+	public function testDatetypeCanBeNow() {
+		$model = $this->buildMockModel();
+		$model->datetype(\DataModeler\Model::DATETYPE_NOW);
+		
+		$this->assertEquals(\DataModeler\Model::DATETYPE_NOW, $model->datetype());
+	}
+	
+	
+	public function testDatetypeCanBeTimestamp() {
+		$model = $this->buildMockModel();
+		$model->datetype(\DataModeler\Model::DATETYPE_TIMESTAMP);
+		
+		$this->assertEquals(\DataModeler\Model::DATETYPE_TIMESTAMP, $model->datetype());
+	}
+	
+	
+	public function testIdIsInitiallyEmpty() {
+		$model = $this->buildMockModel();
+		$this->assertNull($model->id());
+	}
+	
+	
+	public function testIdCanBeSet() {
+		$id = 10;
+		
+		$model = $this->buildMockModel();
+		$model->id($id);
+		
+		$this->assertEquals($id, $model->id());
+	}
+	
 	
 	public function testModelIsInitiallyEmpty() {
 		$model = $this->buildMockModel();
@@ -41,28 +74,48 @@ class ModelTest extends TestCase {
 	}
 	
 	
-	public function testDatetypeIsTimestampByDefault() {
+	public function testTableNameCanBeSet() {
+		$table = 'products';
+	
 		$model = $this->buildMockModel();
-		$model->datetype(100);
+		$model->table($table);
 		
-		$this->assertEquals(\DataModeler\Model::DATETYPE_TIMESTAMP, $model->datetype());
+		$this->assertEquals($table, $model->table()); 
 	}
 	
 	
-	public function testDatetypeCanBeNow() {
+	/**
+	 * @dataProvider providerValidTableNameList
+	 */
+	public function testTableNameCanOnlyContainValidCharacters($table) {
 		$model = $this->buildMockModel();
-		$model->datetype(\DataModeler\Model::DATETYPE_NOW);
+		$model->table($table);
 		
-		$this->assertEquals(\DataModeler\Model::DATETYPE_NOW, $model->datetype());
+		$this->assertEquals($table, $model->table());
 	}
 	
 	
-	public function testDatetypeCanBeTimestamp() {
-		$model = $this->buildMockModel();
-		$model->datetype(\DataModeler\Model::DATETYPE_TIMESTAMP);
+	public function testTableNameHasBackticksRemoved() {
+		$table_with_backticks = '`table_name`';
+		$table_without_backticks = 'table_name';
 		
-		$this->assertEquals(\DataModeler\Model::DATETYPE_TIMESTAMP, $model->datetype());
+		$model = $this->buildMockModel();
+		$model->table($table_with_backticks);
+		
+		$this->assertEquals($table_without_backticks, $model->table());
 	}
 	
 	
+	
+	public function providerValidTableNameList() {
+		return array(
+			array('products'),
+			array('p.products'),
+			array('product_list'),
+			array('product-list'),
+			array('p.product_list'),
+			array('p.product-list')
+		);
+		
+	}
 }
