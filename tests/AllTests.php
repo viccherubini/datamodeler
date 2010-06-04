1561cebc94566ce323cfb06cd4abee36520b2dc7
Vic Cherubini <vmc@leftnode.com>
2010-06-03 21:09:10 -0500

    Added the writer test to the test suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,12 +4,14 @@ declare(encoding='UTF-8');
 namespace DataModelerTest;
 
 require_once 'ModelTest.php';
+require_once 'WriterTest.php';
 
 class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Tests');
 
 		$suite->addTestSuite('\DataModelerTest\ModelTest');
+		$suite->addTestSuite('\DataModelerTest\WriterTest');
 		
 		return $suite;
 	}
