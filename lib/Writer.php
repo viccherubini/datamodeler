92a19fb4494c208b11d41c9a8b882811cbcfa80a
Vic Cherubini <vmc@leftnode.com>
2010-06-04 05:45:01 -0500

    New methods to add an adapter to the stack and two write the model to each adapter set.

--- lib/Writer.php
+++ lib/Writer.php
@@ -17,8 +17,31 @@ class Writer {
 		
 	}
 	
+	
+	public function addAdapter(Adapter $adapter) {
+		$adapter_id = $adapter->getId();
+		if ( false === isset($this->adapterList[$adapter_id]) ) {
+			$this->adapterList[$adapter_id] = $adapter;
+		}
+		return $this;
+	}
+	
+	
 	public function getAdapterList() {
 		return $this->adapterList;
 	}
 	
+	
+	
+	public function write(Model $model) {
+		if ( false !== current($this->adapterList) ) {
+			return false;
+		}
+		
+		foreach ( $this->adapterList as $adapter ) {
+			$adapter->write($model);
+		}
+		
+		return true;
+	}
 }
