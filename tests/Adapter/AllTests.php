97f96940ccbe3bb617e3585f9a57cc061ee0ae50
Vic Cherubini <vmc@leftnode.com>
2010-06-05 06:22:54 -0500

    Changed the addTestSuite() call to use the full namespace path because PHPUnit can't pick it up automatically for some reason.

--- tests/Adapter/AllTests.php
+++ tests/Adapter/AllTests.php
@@ -6,12 +6,12 @@ namespace DataModelerTest\Adapter;
 require_once 'PHPUnit/Framework.php';
 require_once 'Adapter/SqlTest.php';
 
-class Adapter_AllTests {
+class AllTests {
 	
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Adapter Tests');
 		
-		$suite->addTestSuite('SqlTest');
+		$suite->addTestSuite('\DataModelerTest\Adapter\SqlTest');
 		
 		return $suite;
 	}
