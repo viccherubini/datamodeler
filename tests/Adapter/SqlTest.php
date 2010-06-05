e2d9aaa4c1d9a2b968119f87a424199e63589b5d
Vic Cherubini <vmc@leftnode.com>
2010-06-05 05:47:12 -0500

    New test for Sql.

---
+++ tests/Adapter/SqlTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace DataModelerTest\Adapter;
+
+use DataModelerTest\TestCase;
+
+require_once 'lib/Adapter/Pdo.php';
+
+class PdoTest extends TestCase {
+
+	public function testHostNameCanBeSet() {
+		$host = '127.0.0.1';
+		
+		//$pdo_root = 
+		
+		//$pdo = new Pdo;
+		
+		
+	}
+}
