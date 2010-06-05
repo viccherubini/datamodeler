8014d41f6444d81789a5cac283492c23a788e03d
Vic Cherubini <vmc@leftnode.com>
2010-06-05 06:23:46 -0500

    Added the Adapter test suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,6 +3,7 @@
 declare(encoding='UTF-8');
 namespace DataModelerTest;
 
+require_once 'Adapter/AllTests.php';
 require_once 'AdapterTest.php';
 require_once 'ModelTest.php';
 require_once 'WriterTest.php';
@@ -11,6 +12,7 @@ class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Tests');
 
+		$suite->addTestSuite(\DataModelerTest\Adapter\AllTests::suite());
 		$suite->addTestSuite('\DataModelerTest\AdapterTest');
 		$suite->addTestSuite('\DataModelerTest\ModelTest');
 		$suite->addTestSuite('\DataModelerTest\WriterTest');
