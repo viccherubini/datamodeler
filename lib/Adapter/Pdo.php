9ee9a809b29ca314ebd83eb9d685f6737daa4a72
Vic Cherubini <vmc@leftnode.com>
2010-06-05 05:46:09 -0500

    Added basic class structure.

---
+++ lib/Adapter/Pdo.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace DataModeler\Adapter;
+
+class Sql {
+	
+	private $db;
+	
+	public function __construct() {
+		
+	}
+	
+	public function __destruct() {
+		
+		
+	}
+	
+	
+}
