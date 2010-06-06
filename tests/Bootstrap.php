7b07f74c76946f4c258db25b8b270273cdbf9be1
Vic Cherubini <vmc@leftnode.com>
2010-06-06 08:40:49 -0500

    Added some defines for when data needs to be loaded from the datasystem.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -9,3 +9,7 @@ require_once 'TestCase.php';
 $data_modeler_test_path = dirname(__FILE__);
 $data_modeler_lib_path  = $data_modeler_test_path . '/../';
 set_include_path(get_include_path() . PATH_SEPARATOR . $data_modeler_lib_path . PATH_SEPARATOR . $data_modeler_test_path);
+
+define('DS', DIRECTORY_SEPARATOR, false);
+define('DIRECTORY_TESTS', $data_modeler_test_path . DS, false);
+define('DIRECTORY_DATA', DIRECTORY_TESTS . 'data' . DS, false);
