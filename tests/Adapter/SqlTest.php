281f1de320c2922500bbf15efa1f5cf04200669e
Vic Cherubini <vmc@leftnode.com>
2010-06-06 08:42:10 -0500

    Updated test suite for executing raw sql queries thanks to in memory sqlite databases. Not entirely sure I love how this works yet, but I'll work on it.

--- tests/Adapter/SqlTest.php
+++ tests/Adapter/SqlTest.php
@@ -8,16 +8,76 @@ use DataModelerTest\TestCase, DataModeler\Adapter\Sql;
 require_once 'lib/Adapter/Sql.php';
 
 class SqlTest extends TestCase {
+	
+	private $pdo = NULL;
+	
+	private $sql_create_table = NULL;
+	private $sql_list = array();
+	
+	
+	
+	public function setUp() {
+		$this->pdo = new \PDO('sqlite::memory:');
+		
+		$this->sql_create_table = "CREATE TABLE products (id INTEGER PRIMARY KEY, name TEXT, price REAL, sku TEXT)";
+		
+		$this->sql_list = array(
+			"INSERT INTO products VALUES (NULL, 'Product 1', 10.95, 'P1')",
+			"INSERT INTO products VALUES (NULL, 'Product 2', 18.95, 'P2')",
+			"INSERT INTO products VALUES (NULL, 'Product 3', 22.97, 'P3')"
+		);
+		
+		$this->pdo->exec($this->sql_create_table);
+		
+		foreach ( $this->sql_list as $sql ) {
+			$this->pdo->exec($sql);
+		}
+	}
+
+
+	public function tearDown() {
+		$this->pdo = NULL;
+	}
+	
 
 	public function testPdoCanBeAttached() {
-		$pdo = $this->buildMockPdo('sqlite::memory:');
-		
 		$sql = new Sql;
-		$sql->attachDb($pdo);
+		$sql->attachDb($this->pdo);
 		
 		$this->assertTrue($sql->getDb() instanceof \PDO);
 	}
 	
 	
+	/**
+	 * @dataProvider providerRawExecuteQuery
+	 */
+	public function testRawExecuteReturnsTheRowCountOfTheQueryExecuted($sql_query, $expected_row_count) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$actual_row_count = $sql->rawExecute($sql_query);
+		
+		$this->assertEquals($actual_row_count, $expected_row_count);
+	}
+	
+
+	/**
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testRawExecuteDoesNotAllowSelectStatements() {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$sql->rawExecute("SELECT * FROM `products`");
+	}
+
+
+	public function providerRawExecuteQuery() {
+		return array(
+			array("UPDATE `products` SET price = price + 10.50 WHERE sku = 'P1'", 1),
+			array("DELETE FROM `products` WHERE sku = 'P1'", 1),
+			array("INSERT INTO `products` VALUES(NULL, 'Product 4', 58.93, 'P4')", 1)
+		);
+	}
 	
 }
