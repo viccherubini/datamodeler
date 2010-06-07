c1d1fae163817c19ff297ea8288f9f166804ca77
Vic Cherubini <vmc@leftnode.com>
2010-06-07 07:05:22 -0500

    Refactored some of the public methods to use private methods to be more expressive, and started to work on the main query() method.

--- lib/Adapter/Sql.php
+++ lib/Adapter/Sql.php
@@ -7,7 +7,11 @@ require_once 'lib/Exception.php';
 
 class Sql {
 	
-	private $db;
+	private $db = NULL;
+	private $sql = NULL;
+	private $statement = NULL;
+	private $statement_execute = false;
+	
 	
 	public function __construct() {
 		
@@ -20,13 +24,35 @@ class Sql {
 	}
 	
 	public function attachDb(\PDO $db) {
+		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+		
 		$this->db = $db;
 		return $this;
 	}
 	
 	
-	public function rawQuery($sql) {
+	public function query($sql, $input_parameters = array(), $driver_options = array()) {
+		$this->sql = $sql;
+		
+		$this->hasSql();
 		$this->hasDb();
+		
+		$this->statement = $this->pdo->prepare($sql, $driver_options);
+		$this->handlePreparedStatementResult();
+		
+		$this->statement_execute = $this->statement->execute($input_parameters);
+		$this->handlePreparedStatementExecute();
+		
+		return (clone $this->statement);
+	}
+	
+	
+	public function rawQuery($sql) {
+		$this->sql = $sql;
+		
+		$this->hasSql();
+		$this->hasDb();
+		
 		$result = $this->getDb()->query($sql, \PDO::FETCH_ASSOC);
 		
 		return $result;
@@ -34,8 +60,11 @@ class Sql {
 	
 	
 	public function rawExecute($sql) {
+		$this->sql = $sql;
+		
+		$this->hasSql();
 		$this->hasDb();
-		$this->preventSelectStatementExecution($sql);
+		$this->preventSelectStatementExecution();
 		
 		$row_count = $this->getDb()->exec($sql);
 		
@@ -49,6 +78,25 @@ class Sql {
 	
 	
 	
+	private function handlePreparedStatementResult() {
+		if ( false === $this->statement ) {
+			$error_info = $this->statement->errorInfo();
+			$error_string = $error_info[2];
+			throw new \DataModeler\Exception("An error occurred when executing {$sql}. Driver said {$error_string}");
+		}
+		
+		return true;
+	}
+	
+	private function handlePreparedStatementExecute() {
+		if ( false === $this->statement_execute ) {
+			throw new \DataModeler\Exception("An error occurred when executing {$sql}. The prepared statement failed to execute.");
+		}
+		
+		return true;
+	}
+	
+	
 	private function hasDb() {
 		if ( NULL === $this->getDb() ) {
 			throw new \DataModeler\Exception("Database object has not yet been attached to Sql Adapter.");
@@ -58,8 +106,16 @@ class Sql {
 	}
 	
 	
-	private function isSelectStatement($sql) {
-		if ( 0 === stripos($sql, 'SELECT') ) {
+	private function hasSql() {
+		if ( true === empty($this->sql) ) {
+			throw new \DataModeler\Exception("The SQL query is empty and can not be executed.");
+		}
+		return true;
+	}
+	
+	
+	private function isSelectStatement() {
+		if ( 0 === stripos($this->sql, 'SELECT') ) {
 			return true;
 		}
 		
@@ -67,8 +123,8 @@ class Sql {
 	}
 
 
-	private function preventSelectStatementExecution($sql) {
-		if ( true === $this->isSelectStatement($sql) ) {
+	private function preventSelectStatementExecution() {
+		if ( true === $this->isSelectStatement() ) {
 			throw new \DataModeler\Exception("SELECT statements will return an incorrect number of rows, use query() or a prepared statement instead.");
 		}
 		
