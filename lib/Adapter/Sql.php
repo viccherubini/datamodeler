f33d37779570a445dbb16e4211a59d7cbe5fcea2
Vic Cherubini <vmc@leftnode.com>
2010-06-09 06:02:17 -0500

    Updated handlePreparedStatementExecute() to get the error info, and fixed syntax errors to use ->sql rather than  in the exception.

--- lib/Adapter/Sql.php
+++ lib/Adapter/Sql.php
@@ -3,25 +3,15 @@
 declare(encoding='UTF-8');
 namespace DataModeler\Adapter;
 
-require_once 'lib/Exception.php';
+use DataModeler\Adapter;
 
-class Sql {
+class Sql extends Adapter {
 	
 	private $db = NULL;
 	private $sql = NULL;
 	private $statement = NULL;
 	private $statement_execute = false;
 	
-	
-	public function __construct() {
-		
-	}
-	
-	
-	public function __destruct() {
-		
-		
-	}
 	
 	public function attachDb(\PDO $db) {
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
@@ -37,7 +27,7 @@ class Sql {
 		$this->hasSql();
 		$this->hasDb();
 		
-		$this->statement = $this->pdo->prepare($sql, $driver_options);
+		$this->statement = $this->db->prepare($sql, $driver_options);
 		$this->handlePreparedStatementResult();
 		
 		$this->statement_execute = $this->statement->execute($input_parameters);
@@ -80,19 +70,20 @@ class Sql {
 	
 	private function handlePreparedStatementResult() {
 		if ( false === $this->statement ) {
-			$error_info = $this->statement->errorInfo();
+			$error_info = $this->db->errorInfo();
 			$error_string = $error_info[2];
-			throw new \DataModeler\Exception("An error occurred when executing {$sql}. Driver said {$error_string}");
+			throw new \DataModeler\Exception("An error occurred when executing {$this->sql}. Driver said {$error_string}.");
 		}
-		
 		return true;
 	}
+
 	
 	private function handlePreparedStatementExecute() {
 		if ( false === $this->statement_execute ) {
-			throw new \DataModeler\Exception("An error occurred when executing {$sql}. The prepared statement failed to execute.");
+			$error_info = $this->statement->errorInfo();
+			$error_string = $error_info[2];
+			throw new \DataModeler\Exception("An error occurred when executing {$this->sql}. The prepared statement failed to execute. Driver said {$error_string}.");
 		}
-		
 		return true;
 	}
 	
@@ -101,7 +92,6 @@ class Sql {
 		if ( NULL === $this->getDb() ) {
 			throw new \DataModeler\Exception("Database object has not yet been attached to Sql Adapter.");
 		}
-		
 		return true;
 	}
 	
@@ -118,7 +108,6 @@ class Sql {
 		if ( 0 === stripos($this->sql, 'SELECT') ) {
 			return true;
 		}
-		
 		return false;
 	}
 
@@ -127,7 +116,6 @@ class Sql {
 		if ( true === $this->isSelectStatement() ) {
 			throw new \DataModeler\Exception("SELECT statements will return an incorrect number of rows, use query() or a prepared statement instead.");
 		}
-		
 		return true;
 	}
 }
