49b5dc60ad85079758cada0358966581ed605923
Vic Cherubini <vmc@leftnode.com>
2010-06-15 06:59:08 -0500

    Totally updated test class for the \DataModeler\Adapter\Sql class now that the public API is a bit more fleshed out. Changed all underscored variable names to camelCase.

--- tests/Adapter/SqlTest.php
+++ tests/Adapter/SqlTest.php
@@ -16,11 +16,10 @@ class SqlTest extends TestCase {
 	public function setUp() {
 		$this->pdo = new \PDO('sqlite::memory:');
 		
-		$sql_file = DIRECTORY_DATA . 'SqlTest.sql';
-		if ( true === is_file($sql_file) ) {
-			$sql_data = file_get_contents($sql_file);
-			
-			$this->pdo->exec($sql_data);
+		$sqlFile = DIRECTORY_DATA . 'SqlTest.sql';
+		if ( true === is_file($sqlFile) ) {
+			$sqlData = @file_get_contents($sqlFile);
+			$this->pdo->exec($sqlData);
 		}
 	}
 
@@ -37,77 +36,32 @@ class SqlTest extends TestCase {
 		$this->assertTrue($attached instanceof Sql);
 	}
 	
-	
-	/**
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function _testQuery_RequiresQuery() {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$sql->query(NULL);
-	}
-	
-	
-	/**
-	 * @dataProvider providerPreparedQuery
-	 */
-	public function _testQuery_ReturnsIterator($query, $input_parameters) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$iterator = $sql->query($query, $input_parameters);
-		
-		$this->assertIterator($iterator);
-	}
-	
-	/**
-	 * @dataProvider providerInvalidPreparedQuery
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function _testQuery_ThrowsErrorOnInvalidQuery($query) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$sql->query($query);
-	}
-	
-	
-	/**
-	 * @dataProvider providerPreparedQueryWithInvalidInputParameters
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function _testQuery_ThrowsErrorOnInvalidParameters($query, $input_parameters) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$sql->query($query, $input_parameters);
-	}
-	
-	
 	/**
 	 * @expectedException \DataModeler\Exception
 	 */
 	public function testRawExecute_RequiresPdo() {
 		$sql = new Sql;
-		
 		$sql->rawExecute("SELECT * FROM `fake_table` WHERE id = 10");
 	}
 	
+	/**
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testRawExecute_RequiresSql() {
+		$sql = new Sql;
+		$sql->rawExecute(NULL);
+	}
 	
 	/**
 	 * @dataProvider providerRawExecuteQuery
 	 */
-	public function testRawExecute_ReturnsRowCount($sql_query, $expected_row_count) {
+	public function testRawExecute_ReturnsRowCount($sql_query, $expectedRowCount) {
 		$sql = new Sql;
 		$sql->attachDb($this->pdo);
 		
-		$actual_row_count = $sql->rawExecute($sql_query);
-		
-		$this->assertEquals($actual_row_count, $expected_row_count);
+		$this->assertEquals($sql->rawExecute($sql_query), $expectedRowCount);
 	}
 	
-
 	/**
 	 * @expectedException \DataModeler\Exception
 	 */
@@ -118,30 +72,141 @@ class SqlTest extends TestCase {
 		$sql->rawExecute("SELECT * FROM `products`");
 	}
 
+	/**
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFind_RequiresDb() {
+		$sql = new Sql;
+		$sql->find($this->buildMockProduct());
+	}
+	
+	/**
+	 * @dataProvider providerInvalidWhere
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFind_CannotPrepareInvalidQuery($where) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$sql->find($this->buildMockProduct(), $where);
+	}
 
+	/**
+	 * @dataProvider providerInvalidInputParameters
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFind_CannotExecuteInvalidInputParameters($where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$sql->find($this->buildMockProduct(), $where, $inputParameters);
+	}
+	
+	/**
+	 * @dataProvider providerValidWhereAndInputParameters
+	 */
+	public function testFind_ReturnsExistingModelObject($where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$product = $sql->find($this->buildMockProduct(), $where, $inputParameters);
+		
+		$this->assertModel($product);
+		$this->assertTrue($product->exists());
+	}
 
+	/**
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFindAll_RequiresDb() {
+		$sql = new Sql;
+		$sql->findAll($this->buildMockProduct());
+	}
+	
+	/**
+	 * @dataProvider providerInvalidWhere
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFindAll_CannotPrepareInvalidQuery($where) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$sql->findAll($this->buildMockProduct(), $where);
+	}
 
-
+	/**
+	 * @dataProvider providerInvalidInputParameters
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFindAll_CannotExecuteInvalidInputParameters($where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$sql->findAll($this->buildMockProduct(), $where, $inputParameters);
+	}
+	
+	/**
+	 * @dataProvider providerValidWhereAndInputParameters
+	 */
+	public function testFindAll_ReturnsIterator($where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachDb($this->pdo);
+		
+		$iterator = $sql->findAll($this->buildMockProduct(), $where, $inputParameters);
+		
+		$this->assertIterator($iterator);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 	public function providerRawExecuteQuery() {
 		return array(
 			array("UPDATE `products` SET price = price + 10.50 WHERE sku = 'P1'", 1),
 			array("DELETE FROM `products` WHERE sku = 'P1'", 1),
-			array("INSERT INTO `products` VALUES(NULL, 'Product 4', 58.93, 'P4')", 1)
+			array("INSERT INTO `products` VALUES(NULL, 'Product 4', 58.93, 'P4')", 1),
+			array("INSERT INTO `products` VALUES(NULL, 'Product''s 5', 56.24, 'P5')", 1)
 		);
 	}
 	
-	
-	public function providerInvalidPreparedQuery() {
+	public function providerInvalidWhere() {
 		return array(
-			array("SELECT * FROM `invalid_table` WHERE id = :id"),
-			array("SELECT * FROM `products` WHERE not_id = :name"),
-			array("UPDATE `products` SET invalid_name = :name WHERE id = :id")
+			array("id = :i-d"),
+			array("not_id = :name"),
+			array("products = :Array AND face = :book OR tables.field = :table.field")
 		);
 	}
 	
+	public function providerInvalidInputParameters() {
+		return array(
+			array("id = :id", array(':invalid_field' => 'Field Value')),
+			array("id = :id", array(':name' => 'Product 1')),
+			array("sku = :sku AND id = :id", array(':sku' => 'P2', ':id' => 2, ':price' => 11.99))
+		);
+	}
 	
+	public function providerValidWhereAndInputParameters() {
+		return array(
+			array(NULL, array()),
+			array("id = :id", array(':id' => 1)),
+			array("id = :id", array('id' => 1)),
+			array("name = :name", array(':name' => "Product 1")),
+			array("name = :name", array('name' => "Product 1")),
+			array("price = :price", array(':price' => 10.95)),
+			array("price = :price", array('price' => 10.95)),
+			array("price > :price AND sku != :sku", array(':price' => 8.95, ':sku' => 'P2')),
+			array("price > :price AND sku != :sku", array(':price' => 8.95, 'sku' => 'P2')),
+			array("price > :price AND sku != :sku", array('price' => 8.95, ':sku' => 'P2'))
+		);
+	}
+	
+	/*
 	public function providerPreparedQueryWithInvalidInputParameters() {
 		return array(
 			array("SELECT * FROM `products` WHERE id = :id", array(':name' => 'Vic Cherubini')),
@@ -158,5 +223,5 @@ class SqlTest extends TestCase {
 			array("SELECT * FROM `products` WHERE name = :name", array(':name' => "Baba O'Reilly")),
 			array("UPDATE `products` SET name = :name WHERE id = :id", array(':name' => "Baba O'Reilly", ':id' => 1))
 		);
-	}
+	}*/
 }
