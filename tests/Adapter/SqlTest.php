67144a768c634b4f991b47227591b9e8c78cdb5c
Vic Cherubini <vmc@leftnode.com>
2010-06-16 06:58:55 -0500

    Totally rewritten SQL adapter test suite.

--- tests/Adapter/SqlTest.php
+++ tests/Adapter/SqlTest.php
@@ -12,6 +12,7 @@ require_once 'lib/Adapter/Sql.php';
 class SqlTest extends TestCase {
 	
 	private $pdo = NULL;
+	private $product = NULL;
 	
 	public function setUp() {
 		$this->pdo = new \PDO('sqlite::memory:');
@@ -21,207 +22,82 @@ class SqlTest extends TestCase {
 			$sqlData = @file_get_contents($sqlFile);
 			$this->pdo->exec($sqlData);
 		}
+		
+		$this->product = $this->buildMockProduct();
 	}
-
 
 	public function tearDown() {
 		$this->pdo = NULL;
 	}
-	
 
-	public function testAttachDb_CanAttachPdoObject() {
+
+	public function testAttachPdo_CanAttachPdo() {
 		$sql = new Sql;
-		$attached = $sql->attachDb($this->pdo);
-	
-		$this->assertTrue($attached instanceof Sql);
-	}
-	
-	/**
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testRawExecute_RequiresPdo() {
-		$sql = new Sql;
-		$sql->rawExecute("SELECT * FROM `fake_table` WHERE id = 10");
-	}
-	
-	/**
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testRawExecute_RequiresSql() {
-		$sql = new Sql;
-		$sql->rawExecute(NULL);
-	}
-	
-	/**
-	 * @dataProvider providerRawExecuteQuery
-	 */
-	public function testRawExecute_ReturnsRowCount($sql_query, $expectedRowCount) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
 		
-		$this->assertEquals($sql->rawExecute($sql_query), $expectedRowCount);
-	}
-	
-	/**
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testRawExecute_DoesNotAllowSelectStatements() {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$sql->rawExecute("SELECT * FROM `products`");
+		$this->assertSql($sql->attachPdo($this->pdo));
 	}
 
 	/**
 	 * @expectedException \DataModeler\Exception
 	 */
-	public function testFind_RequiresDb() {
+	public function testPrepare_RequiresPdo() {
 		$sql = new Sql;
-		$sql->find($this->buildMockProduct());
+		
+		$sql->prepare($this->product);
 	}
 	
-	/**
-	 * @dataProvider providerInvalidWhere
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testFind_CannotPrepareInvalidQuery($where) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
+	public function testPrepare_OnlyPreparesOnceForSameModel() {
+		$product1 = $this->buildMockProduct();
+		$product2 = $this->buildMockProduct();
 		
-		$sql->find($this->buildMockProduct(), $where);
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$sql->prepare($product1);
+		$sql->prepare($product2);
+		
+		$this->assertEquals(1, $sql->getPrepareCount());
+	}
+	
+	public function testPrepare_PreparesOnceForEachDifferentModel() {
+		$product1 = $this->buildMockProduct();
+		$product2 = $this->buildMockProduct();
+		$user1 = $this->buildMockModel('users', 'id');
+		$user2 = $this->buildMockModel('users', 'id');
+		
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$sql->prepare($product1);
+		$sql->prepare($product2);
+		$sql->prepare($user1);
+		$sql->prepare($user2);
+		
+		$this->assertEquals(2, $sql->getPrepareCount());
 	}
 
-	/**
-	 * @dataProvider providerInvalidInputParameters
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testFind_CannotExecuteInvalidInputParameters($where, $inputParameters) {
+	public function testGet_ReturnsExistingModelIfFound() {
 		$sql = new Sql;
-		$sql->attachDb($this->pdo);
+		$sql->attachPdo($this->pdo);
 		
-		$sql->find($this->buildMockProduct(), $where, $inputParameters);
-	}
-	
-	/**
-	 * @dataProvider providerValidWhereAndInputParameters
-	 */
-	public function testFind_ReturnsExistingModelObject($where, $inputParameters) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$product = $sql->find($this->buildMockProduct(), $where, $inputParameters);
+		$sql->prepare($this->product);
+		$product = $sql->get(1);
 		
 		$this->assertModel($product);
 		$this->assertTrue($product->exists());
 	}
-
-	/**
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testFindAll_RequiresDb() {
+	
+	public function testGet_ReturnsNonExistingModelIfNotFound() {
 		$sql = new Sql;
-		$sql->findAll($this->buildMockProduct());
-	}
-	
-	/**
-	 * @dataProvider providerInvalidWhere
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testFindAll_CannotPrepareInvalidQuery($where) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
+		$sql->attachPdo($this->pdo);
 		
-		$sql->findAll($this->buildMockProduct(), $where);
-	}
-
-	/**
-	 * @dataProvider providerInvalidInputParameters
-	 * @expectedException \DataModeler\Exception
-	 */
-	public function testFindAll_CannotExecuteInvalidInputParameters($where, $inputParameters) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
+		$sql->prepare($this->product);
+		$product = $sql->get(10000);
 		
-		$sql->findAll($this->buildMockProduct(), $where, $inputParameters);
-	}
-	
-	/**
-	 * @dataProvider providerValidWhereAndInputParameters
-	 */
-	public function testFindAll_ReturnsIterator($where, $inputParameters) {
-		$sql = new Sql;
-		$sql->attachDb($this->pdo);
-		
-		$iterator = $sql->findAll($this->buildMockProduct(), $where, $inputParameters);
-		
-		$this->assertIterator($iterator);
+		$this->assertModel($product);
+		$this->assertFalse($product->exists());
 	}
 	
 	
 	
-	
-	
-	
-	
-	
-
-
-	public function providerRawExecuteQuery() {
-		return array(
-			array("UPDATE `products` SET price = price + 10.50 WHERE sku = 'P1'", 1),
-			array("DELETE FROM `products` WHERE sku = 'P1'", 1),
-			array("INSERT INTO `products` VALUES(NULL, 'Product 4', 58.93, 'P4')", 1),
-			array("INSERT INTO `products` VALUES(NULL, 'Product''s 5', 56.24, 'P5')", 1)
-		);
-	}
-	
-	public function providerInvalidWhere() {
-		return array(
-			array("id = :i-d"),
-			array("not_id = :name"),
-			array("products = :Array AND face = :book OR tables.field = :table.field")
-		);
-	}
-	
-	public function providerInvalidInputParameters() {
-		return array(
-			array("id = :id", array(':invalid_field' => 'Field Value')),
-			array("id = :id", array(':name' => 'Product 1')),
-			array("sku = :sku AND id = :id", array(':sku' => 'P2', ':id' => 2, ':price' => 11.99))
-		);
-	}
-	
-	public function providerValidWhereAndInputParameters() {
-		return array(
-			array(NULL, array()),
-			array("id = :id", array(':id' => 1)),
-			array("id = :id", array('id' => 1)),
-			array("name = :name", array(':name' => "Product 1")),
-			array("name = :name", array('name' => "Product 1")),
-			array("price = :price", array(':price' => 10.95)),
-			array("price = :price", array('price' => 10.95)),
-			array("price > :price AND sku != :sku", array(':price' => 8.95, ':sku' => 'P2')),
-			array("price > :price AND sku != :sku", array(':price' => 8.95, 'sku' => 'P2')),
-			array("price > :price AND sku != :sku", array('price' => 8.95, ':sku' => 'P2'))
-		);
-	}
-	
-	/*
-	public function providerPreparedQueryWithInvalidInputParameters() {
-		return array(
-			array("SELECT * FROM `products` WHERE id = :id", array(':name' => 'Vic Cherubini')),
-			array("SELECT * FROM `products` WHERE id = :name", array(':id' => 'Vic Cherubini')),
-			array("UPDATE `products` SET name = :name WHERE id = :id", array(':xname' => 'Vic Cherubini'))
-		);
-	}
-	
-	
-	public function providerPreparedQuery() {
-		return array(
-			array("SELECT * FROM `products` WHERE id = :id", array(':id' => 2)),
-			array("SELECT * FROM `products` WHERE id = :id AND sku = :sku", array(':id' => 2, ':sku' => 'P2')),
-			array("SELECT * FROM `products` WHERE name = :name", array(':name' => "Baba O'Reilly")),
-			array("UPDATE `products` SET name = :name WHERE id = :id", array(':name' => "Baba O'Reilly", ':id' => 1))
-		);
-	}*/
 }
