c5c3ec96a907295d64ec0294357376644677ae88
Vic Cherubini <vmc@leftnode.com>
2010-06-17 05:59:01 -0500

    Added all of the tests for the new Sql class methods.

--- tests/Adapter/SqlTest.php
+++ tests/Adapter/SqlTest.php
@@ -4,6 +4,7 @@ declare(encoding='UTF-8');
 namespace DataModelerTest\Adapter;
 
 use DataModelerTest\TestCase,
+	DataModeler\Model,
 	DataModeler\Adapter\Sql,
 	DataModeler\Iterator;
 
@@ -13,6 +14,7 @@ class SqlTest extends TestCase {
 	
 	private $pdo = NULL;
 	private $product = NULL;
+	private $user = NULL;
 	
 	public function setUp() {
 		$this->pdo = new \PDO('sqlite::memory:');
@@ -24,17 +26,44 @@ class SqlTest extends TestCase {
 		}
 		
 		$this->product = $this->buildMockProduct();
+		$this->user = $this->buildMockUser();
 	}
 
 	public function tearDown() {
 		$this->pdo = NULL;
 	}
 
-
 	public function testAttachPdo_CanAttachPdo() {
 		$sql = new Sql;
 		
 		$this->assertSql($sql->attachPdo($this->pdo));
+	}
+	
+	public function testGetStatement_ReturnsPdoStatement() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$sql->prepare($this->product);
+		$sql->get(1);
+		
+		$this->assertTrue($sql->getStatement() instanceof \PDOStatement);
+	}
+	
+	public function testGetQueryString_ReturnsUnparsedQuery() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$sql->prepare($this->product);
+		$sql->get(1);
+		
+		$this->assertType('string', $sql->getQueryString());
+	}
+	
+	public function testGetQueryString_ReturnsNullWhenNoQuery() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$this->assertNull($sql->getQueryString());
 	}
 
 	/**
@@ -46,7 +75,7 @@ class SqlTest extends TestCase {
 		$sql->prepare($this->product);
 	}
 	
-	public function testPrepare_OnlyPreparesOnceForSameModel() {
+	public function testPrepare_PreparesOnceForSameModel() {
 		$product1 = $this->buildMockProduct();
 		$product2 = $this->buildMockProduct();
 		
@@ -62,8 +91,8 @@ class SqlTest extends TestCase {
 	public function testPrepare_PreparesOnceForEachDifferentModel() {
 		$product1 = $this->buildMockProduct();
 		$product2 = $this->buildMockProduct();
-		$user1 = $this->buildMockModel('users', 'id');
-		$user2 = $this->buildMockModel('users', 'id');
+		$user1 = $this->buildMockUser();
+		$user2 = $this->buildMockUser();
 		
 		$sql = new Sql;
 		$sql->attachPdo($this->pdo);
@@ -76,7 +105,7 @@ class SqlTest extends TestCase {
 		$this->assertEquals(2, $sql->getPrepareCount());
 	}
 
-	public function testGet_ReturnsExistingModelIfFound() {
+	public function testGet_ReturnsModelIfFound() {
 		$sql = new Sql;
 		$sql->attachPdo($this->pdo);
 		
@@ -87,7 +116,7 @@ class SqlTest extends TestCase {
 		$this->assertTrue($product->exists());
 	}
 	
-	public function testGet_ReturnsNonExistingModelIfNotFound() {
+	public function testGet_ReturnsEmptyModelIfNotFound() {
 		$sql = new Sql;
 		$sql->attachPdo($this->pdo);
 		
@@ -98,6 +127,122 @@ class SqlTest extends TestCase {
 		$this->assertFalse($product->exists());
 	}
 	
+	/**
+	 * @dataProvider providerFindModel
+	 */
+	public function testFind_ReturnsModel(Model $model, $where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$this->assertFalse($model->exists());
+		
+		$sql->prepare($model, $where);
+		$model = $sql->find($inputParameters);
+		
+		$this->assertModel($model);
+		$this->assertTrue($model->exists());
+	}
 	
+	/**
+	 * @dataProvider providerFindModel
+	 */
+	public function testFindAll_ReturnsListOfModels(Model $model, $where, $inputParameters) {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$this->assertFalse($model->exists());
+		
+		$sql->prepare($model, $where);
+		$modelList = $sql->findAll($inputParameters);
+		
+		$this->assertModelList($modelList);
+	}
 	
+	public function testSave_SetsInsertId() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$product = $this->buildMockProduct();
+		$product->setName('Product 1');
+		$product->setPrice(10.99);
+		
+		$product = $sql->save($product);
+		
+		$this->assertGreaterThan(0, $product->id());
+	}
+	
+	public function testSave_PreparesOnceForSameModel() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		for ( $i=0; $i<10; $i++ ) {
+			$price = round(mt_rand(1, 25) + (mt_rand(1, 25) / mt_rand(1, 25)), 2);
+			
+			$product = $this->buildMockProduct();
+			$product->setName("Product {$i}");
+			$product->setPrice($price);
+			$product->setSku("P{$i}");
+			
+			$sql->save($product);
+		}
+		
+		$this->assertEquals(1, $sql->getPrepareCount());
+	}
+	
+	public function testSave_PreparesTwiceForDifferentModels() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$product = $this->buildMockProduct();
+		$product->setName('Product 4');
+		$product->setPrice(10.99);
+		$product->setSku('P4');
+		
+		$user = $this->buildMockUser();
+		$user->setUsername('leftnode');
+		$user->setPassword('password_test');
+		
+		$sql->save($product);
+		$sql->save($user);
+		
+		$this->assertEquals(2, $sql->getPrepareCount());
+	}
+	
+	public function testSave_PreparesTwiceForDifferentSaveMethods() {
+		$sql = new Sql;
+		$sql->attachPdo($this->pdo);
+		
+		$product1 = $this->buildMockProduct();
+		$product1->id(1);
+		$product1->setName('Product 1 *Updated*');
+		$product1->setPrice(42.56);
+		$product1->setSku('P1_NEW');
+		
+		$product2 = $this->buildMockProduct();
+		$product2->setName('Product 2');
+		$product2->setPrice(8.56);
+		$product2->setSku('P5');
+		
+		$sql->save($product1); // update
+		$sql->save($product2); // insert
+		
+		$this->assertEquals(2, $sql->getPrepareCount());
+	}
+	
+	public function providerFindModel() {
+		$user = $this->buildMockUser();
+		$product = $this->buildMockProduct();
+		
+		return array(
+			array($product, 'id = :id', array('id' => 1)),
+			array($product, 'id = ?', array(1)),
+			array($product, 'id = :id OR sku = :sku', array('id' => 1, 'sku' => 'P2')),
+			array($product, 'id = :id AND sku = :sku', array('id' => 1, 'sku' => 'P1')),
+			array($product, 'price > :price', array('price' => 10.99)),
+			array($product, 'name = :name', array('name' => 'Product 1')),
+			array($user, 'username = :username', array('username' => 'vcherubini')),
+			array($user, 'username = :username AND password = :password', array('username' => 'vcherubini', 'password' => 'password1')),
+			array($user, 'age = :age AND favorite_book = :favorite_book', array('age' => 25, 'favorite_book' => 'xUnit Test Patterns'))
+		);
+	}
 }
