1a3800642ec84d2843096c3b38fa261bd0ad17b8
Vic Cherubini <vmc@leftnode.com>
2010-06-17 06:00:13 -0500

    Added new assertion method assertModelList() to determine if an array is an array of \DataModeler\Model objects, and updated the other custom assertion methods to pass the message around to each. Also added new method buildMockUser() to do that since users are in the SqlData.sql file now.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,40 +13,42 @@ class TestCase extends \PHPUnit_Framework_TestCase {
 		self::assertThat(is_array($a), self::isTrue(), $message);
 	}
 	
-	
 	public static function assertEmptyArray($a, $message = '') {
 		self::assertArray($a);
 		self::assertEquals(0, count($a), $message);
 	}
 	
-	
 	public static function assertNotEmptyArray($a, $message = '') {
-		self::assertArray($a);
+		self::assertArray($a, $message);
 		self::assertGreaterThan(0, count($a), $message);
 	}
 
+	public static function assertModelList($modelList, $message = '') {
+		self::assertArray($modelList, $message);
+		foreach ( $modelList as $model ) {
+			self::assertTrue($model instanceof Model, $message);
+		}
+	}
 	
 	public static function assertIterator($obj, $message = '') {
-		self::assertTrue(is_object($obj));
-		self::assertTrue($obj instanceof Iterator);
+		self::assertTrue(is_object($obj), $message);
+		self::assertTrue($obj instanceof Iterator, $message);
 	}
 
 	public static function assertModel($obj, $message = '') {
-		self::assertTrue(is_object($obj));
-		self::assertTrue($obj instanceof Model);
+		self::assertTrue(is_object($obj), $message);
+		self::assertTrue($obj instanceof Model, $message);
 	}
 
 	public static function assertSql($obj, $message = '') {
-		self::assertTrue(is_object($obj));
-		self::assertTrue($obj instanceof Sql);
+		self::assertTrue(is_object($obj), $message);
+		self::assertTrue($obj instanceof Sql, $message);
 	}
-
 	
 	protected function buildMockAdapter() {
 		$adapter = $this->getMockForAbstractClass('\DataModeler\Adapter');
 		return $adapter;
 	}
-
 	
 	protected function buildMockModel($table = NULL, $pkey = NULL) {
 		$model = $this->getMockForAbstractClass('\DataModeler\Model');
@@ -58,6 +60,11 @@ class TestCase extends \PHPUnit_Framework_TestCase {
 	
 	protected function buildMockProduct() {
 		$model = $this->buildMockModel('products', 'id');
+		return $model;
+	}
+	
+	protected function buildMockUser() {
+		$model = $this->buildMockModel('users', 'id');
 		return $model;
 	}
 	
