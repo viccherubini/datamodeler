900ee2753ed31515e08c7cf06d0c482e9b4e5af9
Vic Cherubini <vmc@leftnode.com>
2010-06-20 08:46:19 -0500

    Added the large_object table to the testsuite.

--- tests/.Data/SqlTest-sqlite.sql
+++ tests/.Data/SqlTest-sqlite.sql
@@ -4,10 +4,12 @@ INSERT INTO products VALUES (NULL, 'Product 1', 10.95, 'P1');
 INSERT INTO products VALUES (NULL, 'Product 2', 18.95, 'P2');
 INSERT INTO products VALUES (NULL, 'Product 3', 22.97, 'P3');
 
-
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, age INTEGER, favorite_book TEXT);
 INSERT INTO users VALUES(NULL, 'vcherubini', 'password1', 25, 'xUnit Test Patterns');
 INSERT INTO users VALUES(NULL, 'bsaget', 'password2', 50, 'The Olsen Twins: A Legacy');
 INSERT INTO users VALUES(NULL, 'ggottfried', 'password3', 52, 'Its The Whiskey Talkin');
 INSERT INTO users VALUES(NULL, 'howard_stern', 'password4', 56, 'Private Parts');
+
+DROP TABLE IF EXISTS large_object;
+CREATE TABLE large_object (large_object_id INTEGER PRIMARY KEY, object_data TEXT);
