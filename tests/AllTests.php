d2163dae7691cef495faaa9c7e72b83994635659
Vic Cherubini <vmc@leftnode.com>
2010-06-25 10:40:32 -0500

    Added the Type test suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,10 +4,13 @@ declare(encoding='UTF-8');
 namespace DataModelerTest;
 
 require_once 'Adapter/AllTests.php';
+require_once 'Type/AllTests.php';
+
 require_once 'AdapterTest.php';
 require_once 'IteratorTest.php';
 require_once 'LoaderTest.php';
 require_once 'ModelTest.php';
+require_once 'TypeTest.php';
 require_once 'WriterTest.php';
 
 class AllTests {
@@ -15,10 +18,13 @@ class AllTests {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Tests');
 
 		$suite->addTestSuite(\DataModelerTest\Adapter\AllTests::suite());
+		$suite->addTestSuite(\DataModelerTest\Type\AllTests::suite());
+		
 		$suite->addTestSuite('\DataModelerTest\AdapterTest');
 		$suite->addTestSuite('\DataModelerTest\IteratorTest');
 		$suite->addTestSuite('\DataModelerTest\LoaderTest');
 		$suite->addTestSuite('\DataModelerTest\ModelTest');
+		$suite->addTestSuite('\DataModelerTest\TypeTest');
 		$suite->addTestSuite('\DataModelerTest\WriterTest');
 		
 		return $suite;
