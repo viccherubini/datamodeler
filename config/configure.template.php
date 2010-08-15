8c7e816bcfdbb3f8b084ba75305eaeaf3661e3e4
Vic Cherubini <vmc@leftnode.com>
2010-08-14 21:11:53 -0500

    Removed the configuration file and updated the .gitignore file to reflect that.

--- config/configure.template.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('SERVER_HOST', '', false);
-define('STORE_USERNAME', '', false);
-define('STORE_PASSWORD', '', false);
-define('STORE_NAME', '', false);
-define('STORE_PORT', '', false);
