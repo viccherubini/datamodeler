77bdb4fa0751aa4c022d627c3c7ec4cea717ae5d
Vic Cherubini <vmc@leftnode.com>
2010-08-17 04:56:17 -0500

    Updated the constructor to attempt to pull in the name of the class and function that the error occurred in.

--- DataModeler/Exception.php
+++ DataModeler/Exception.php
@@ -5,4 +5,25 @@ namespace DataModeler;
 
 class Exception extends \Exception {
 
+	public function __construct($message) {
+		$messageBits = array();
+		
+		$trace = $this->getTrace();
+		$trace = current($trace);
+		
+		if ( isset($trace['class']) ) {
+			$messageBits[] = $trace['class'];
+		}
+		
+		$function = NULL;
+		if ( isset($trace['function']) ) {
+			$messageBits[] = $trace['function'];
+		}
+
+		$messageBits[] = $message;
+		$message = implode('_', $messageBits);
+		
+		parent::__construct($message);
+	}
+
 }
