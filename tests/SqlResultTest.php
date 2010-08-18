d8e0a7ea32f5f28865c65ce3f6749e5c51c34be5
Vic Cherubini <vmc@leftnode.com>
2010-08-18 07:01:32 -0500

    Tests

--- tests/SqlResultTest.php
+++ tests/SqlResultTest.php
@@ -11,8 +11,6 @@ require_once 'DataModeler/SqlResult.php';
 
 class SqlResultTest extends TestCase {
 	
-	
-	
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 */
@@ -24,9 +22,71 @@ class SqlResultTest extends TestCase {
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 */
-	public function testAttachStatement_MustBeModelObject() {
+	public function testAttachPdoStatement_MustBePdoStatementObject() {
 		$sqlResult = new SqlResult;
-		$sqlResult->attachStatement(NULL);
+		$sqlResult->attachPdoStatement(NULL);
+	}
+	
+	/**
+	 * @dataProvider providerFindParameters
+	 */
+	public function testFindFirst_ReturnsNvpArrayWithSingleParameter($parameters) {
+		$this->buildPdo();
+		$statement = $this->pdo->prepare('SELECT * FROM products WHERE product_id = ?');
+		
+		$sqlResult = new SqlResult;
+		$sqlResult->attachPdoStatement($statement);
+
+		$row = $sqlResult->findFirst($parameters);
+		
+		$this->assertTrue(is_array($row));
+		$this->assertGreaterThan(0, count($row));
+	}
+	
+	public function _testFindFirst_ReturnsNvpArrayWithMultipleParameters() {
+		$this->buildPdo();
+		$statement = $this->pdo->prepare('SELECT * FROM products WHERE product_id = ? AND price > ?');
+		
+		$sqlResult = new SqlResult;
+		$sqlResult->attachPdoStatement($statement);
+
+		$row = $sqlResult->findFirst(1, 8.00);
+	
+		$this->assertGreaterThan(0, count($row));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * @expectedException \DataModeler\Exception
+	 */
+	public function testFree_CanNotFind() {
+		$this->buildPdo();
+		$statement = $this->pdo->prepare('SELECT * FROM products WHERE product_id = ?');
+		
+		$sqlResult = new SqlResult;
+		$sqlResult->attachPdoStatement($statement);
+		
+		$sqlResult->findFirst(1);
+		$sqlResult->free();
+		
+		$sqlResult->findFirst(1);
+	}
+	
+	public function providerFindParameters() {
+		$obj = new \stdClass;
+		$obj->product_id = 1;
+		
+		return array(
+			array(1),
+			array(array(1)),
+			array($obj)
+		);
 	}
 	
 }
