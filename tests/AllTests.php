b460fb27a9875171065ebc7bb0f5905ebae3c339
Vic Cherubini <vmc@leftnode.com>
2010-08-23 07:15:18 -0500

    Removed the LibTest and MiscTest tests from AllTests, and added Exception test.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,9 +3,8 @@
 declare(encoding='UTF-8');
 namespace DataModelerTest;
 
+require_once 'ExceptionTest.php';
 require_once 'IteratorTest.php';
-require_once 'LibTest.php';
-require_once 'MiscTest.php';
 require_once 'ModelTest.php';
 require_once 'SqlTest.php';
 require_once 'SqlResultTest.php';
@@ -14,9 +13,8 @@ class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('DataModeler Tests');
 
+		$suite->addTestSuite('\DataModelerTest\ExceptionTest');
 		$suite->addTestSuite('\DataModelerTest\IteratorTest');
-		$suite->addTestSuite('\DataModelerTest\LibTest');
-		$suite->addTestSuite('\DataModelerTest\MiscTest');
 		$suite->addTestSuite('\DataModelerTest\ModelTest');
 		$suite->addTestSuite('\DataModelerTest\SqlTest');
 		$suite->addTestSuite('\DataModelerTest\SqlResultTest');
