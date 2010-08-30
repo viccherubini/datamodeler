b4f0017c56ad32a6ac9230642a9e2b19dabb730a
Vic Cherubini <vmc@leftnode.com>
2010-08-30 07:14:26 -0500

    buildPdo() sets the ATTR_EMULATE_PREPARES constant to false to ensure that prepares are always sent to the database. Slower, but more correct.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -31,6 +31,7 @@ class TestCase extends \PHPUnit_Framework_TestCase {
 	
 	protected function buildPdo() {
 		$this->pdo = new \PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+		$this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 		
 		if ( empty($this->loadedSql) ) {
 			$sqlFile = DIRECTORY_DATA . DB_TYPE . '.sql';
