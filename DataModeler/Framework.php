8e9c44179fbca62a197e4232ac194fdab64f03b7
Vic Cherubini <vmc@leftnode.com>
2010-09-13 07:11:36 -0500

    Central file to include all of the other files.

---
+++ DataModeler/Framework.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace DataModeler;
+
+require_once 'DataModeler/Exception.php';
+
+require_once 'DataModeler/Iterator.php';
+require_once 'DataModeler/Model.php';
+require_once 'DataModeler/Sql.php';
+require_once 'DataModeler/SqlResult.php';
