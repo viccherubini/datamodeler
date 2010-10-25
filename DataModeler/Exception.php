7b1e073c41d2d0ddd588218e2588777ba9eebead
Vic Cherubini <vmc@leftnode.com>
2010-10-25 08:57:01 -0500

    Wrote a much better Exception handler to gives a detailed but terse stack trace.

--- DataModeler/Exception.php
+++ DataModeler/Exception.php
@@ -7,26 +7,41 @@ class Exception extends \Exception {
 
 	public function __construct($message) {
 		$msg = NULL;
-		
+
 		$trace = $this->getTrace();
-		$trace = current($trace);
-		
-		$filename = parent::getFile();
-		$lineNumber = parent::getLine();
-		
-		if ( isset($trace['class']) ) {
-			$msg = $trace['class'];
-		}
-		
-		if ( isset($trace['type']) ) {
-			$msg .= $trace['type'];
-		}
-		
-		if ( isset($trace['function']) ) {
-			$msg .= $trace['function'] . '()';
+		$traceLength = count($trace);
+
+		$methods = array();
+		for ( $i=$traceLength-1; $i>=0; $i-- ) {
+			$args = $trace[$i]['args'];
+			$argc = count($args);
+
+			$argv = array();
+			for ( $j=0; $j<$argc; $j++ ) {
+				if ( is_object($args[$j]) ) {
+					$argv[] = get_class($args[$j]);
+				} elseif ( is_array($args[$j]) ) {
+					$argv[] = 'array';
+				} elseif ( is_bool($args[$j]) ) {
+					$argv[] = ( false === $args[$j] ? 'false' : 'true' );
+				} elseif ( is_null($args[$j]) ) {
+					$argv[] = 'NULL';
+				} else {
+					$argv[] = $args[$j];
+				}
+			}
+
+			$signature = $trace[$i]['class'] . $trace[$i]['type'] . $trace[$i]['function'];
+			$arguments = implode(',', $argv);
+
+			$methods[] = '[' . $i . '] ' . $signature . '(' . $arguments . ')';
 		}
 
-		$msg .= " [{$message}] ({$filename} +{$lineNumber})";
+		$filename = parent::getFile();
+		$lineNumber = parent::getLine();
+
+		$methodTrace = implode(PHP_EOL, $methods);
+		$msg = $message . PHP_EOL . PHP_EOL . $methodTrace . PHP_EOL . PHP_EOL . $filename . ' +' . $lineNumber;
 
 		parent::__construct($msg);
 	}
